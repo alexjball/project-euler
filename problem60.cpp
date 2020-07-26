e781efde0c3584f89c1c5a2d560f6fae75b3ce0f
Alex Ball <me@alexjball.com>
2020-07-25 21:51:44 -0400

    Complete problem 60

--- problem60.cpp
+++ problem60.cpp
@@ -1,58 +1,109 @@
+#include <bits/stdc++.h>
 #include <cassert>
 #include <cmath>
 #include <iostream>
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-#define MAX_PRIME 1000
-
 /**
  * Find the lowest sum of 5 primes such that any concatination of primes is also
- * prime. Primes with this property form a k-clique.
+ * prime.
+ *
+ * Consider a graph formed by a vertex for each prime and an edge between pairs
+ * of primes that have this property. Sets of larger primes with the
+ * concatenation property form k-cliques in this graph. So we want to find the
+ * 5-clique with the smallest sum.
  *
  * The approach:
- * 1) generate a list of all primes below some max greater than the largest
- * prime in the solution set 2) Find all 2-cliques with brute force (O(n^2)) 3)
- * Find k+1-cliques by iterating over k-cliques and checking for primes (above
- * largest in clique) that form a k+1-clique 4) Proceed from 2 to 5-clique. If
- * there are no solutions, increase the max and start again.
- *
- * There is a clique data structure that stores the set of primes
- * The 2-cliques are stored as an adjacency list.
- *
- * Unverified Assumptions:
- * - We only need to store edges from smaller to larger primes
- * - We only need to check primes that are larger than the largest prime in a
- * clique. If there is a k-clique that can be expanded by adding a smaller
- * prime, then there is an overall-smaller k-clique which will be evaluated
- * later on in the k-iteration
+ * 1) generate a list of all primes below some max (O(n^1.5))
+ * 2) Find all 2-cliques with brute force (O(n^2))
+ * 3) Expand the k-cliques by checking for higher primes that form pairs with
+ *    all members of the k-cliques 4) Repeat until 5 cliques are formed. 5)
+ *    Return the 5-clique with the smallest sum.
  */
-struct Clique {
-  vector<int> el_index;
-};
 
-struct Solution {
+typedef multimap<int, int> MapType;
+typedef vector<int> Clique;
+
+struct Problem60 {
   vector<int> primes;
-  map<int, int> pairs_by_min;
-  vector<vector<int>> concat_primes;
+  MapType pairs_by_min;
+  vector<Clique> cliques;
+
   int maxPrime;
+  int maxCliqueSize;
 
-  Solution(int maxPrime) { this->maxPrime = maxPrime; }
+  Problem60(int maxPrime, int maxCliqueSize) {
+    this->maxPrime = maxPrime;
+    this->maxCliqueSize = maxCliqueSize;
+  }
 
-  void solve() {
+  pair<int, vector<int>> solve() {
     primes = primes_below(maxPrime);
     compute_2_cliques();
+    for (int i = 3; i <= this->maxCliqueSize; i++) {
+      expand_cliques();
+    }
+    return smallest_clique();
+  }
+
+  pair<int, Clique> smallest_clique() {
+    int smallest_sum = INT_MAX;
+    Clique smallest_clique;
+    for (auto &clique : this->cliques) {
+      int sum = accumulate(clique.begin(), clique.end(), 0);
+      if (sum < smallest_sum) {
+        smallest_clique = clique;
+        smallest_sum = sum;
+      }
+    }
+    return pair<int, Clique>(smallest_sum, smallest_clique);
+  }
+
+  void expand_cliques() {
+    vector<Clique> expanded_cliques;
+    for (auto &clique : this->cliques) {
+      MapType current_el_by_candidate;
+      pair<MapType::iterator, MapType::iterator> ret;
+
+      for (int el : clique) {
+        ret = pairs_by_min.equal_range(el);
+        for (MapType::iterator it = ret.first; it != ret.second; ++it) {
+          current_el_by_candidate.insert(pair<int, int>(it->second, it->first));
+        }
+      }
+
+      for (MapType::iterator it = current_el_by_candidate.begin();
+           it != current_el_by_candidate.end();) {
+        int candidate = it->first;
+
+        if (current_el_by_candidate.count(candidate) == clique.size()) {
+          Clique new_clique(clique);
+          new_clique.push_back(candidate);
+          expanded_cliques.push_back(new_clique);
+        }
+
+        do {
+          it++;
+        } while (it != current_el_by_candidate.end() && it->first == candidate);
+      }
+    }
+
+    cliques = expanded_cliques;
   }
 
   void compute_2_cliques() {
+    int p1, p2;
     for (int i = 0; i < primes.size(); i++) {
       for (int j = i + 1; j < primes.size(); j++) {
-        if (is_concat_prime(primes[i], primes[j])) {
-          // primes.push_back();
-          // cliques.push_back(new vector({i, j}));
+        p1 = primes[i], p2 = primes[j];
+        if (is_concat_prime(p1, p2)) {
+          pairs_by_min.insert(pair<int, int>(p1, p2));
+          cliques.push_back(Clique({p1, p2}));
         }
       }
     }
@@ -92,8 +143,12 @@ struct Solution {
 };
 
 int main() {
-  Solution solution(MAX_PRIME);
+  Problem60 solution(10000, 5);
 
-  assert(solution.is_prime(11));
-  assert(!solution.is_prime(10));
+  pair<int, vector<int>> result = solution.solve();
+  cout << "min sum " << result.first << endl << "clique [";
+  for (int i : result.second) {
+    cout << i << " ";
+  }
+  cout << "]" << endl;
 }
