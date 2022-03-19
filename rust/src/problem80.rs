433369898bc6ac15b8eb5d2ef6fd29bd42af1a43
Alex Ball <me@alexjball.com>
2022-03-19 13:04:17 -0400

    problem 80

--- rust/src/problem80.rs
+++ rust/src/problem80.rs
@@ -1,10 +1,26 @@
-use std::{str::FromStr, time::Instant};
+use std::time::Instant;
 
 use bigdecimal::BigDecimal;
 
 pub fn run() {
     let start = Instant::now();
-    let dec = BigDecimal::from_str("5").unwrap().with_prec(200);
-    let sqrt = dec.sqrt().unwrap().with_prec(200);
-    println!("{:?} {} {}", start.elapsed(), dec, sqrt);
+    let sum = (1..=100)
+        .flat_map(|n| {
+            BigDecimal::from(n).sqrt().and_then(|s| {
+                if s.is_integer() {
+                    None
+                } else {
+                    Some(s.as_bigint_and_exponent().0)
+                }
+            })
+        })
+        .map(|digits| {
+            digits.to_string()[..100]
+                .chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .sum::<u32>()
+        })
+        .sum::<u32>();
+
+    println!("{:?} {}", start.elapsed(), sum);
 }
